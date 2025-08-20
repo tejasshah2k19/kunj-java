3d85532a554bce6c0bd67b5735654817dee2389d
Tejas Shah <tejasshah2k19@gmail.com>
2025-08-20 12:16:47 +0530

    br

--- src/ch9io/FileWriteDemo.java
+++ src/ch9io/FileWriteDemo.java
@@ -1,13 +1,14 @@
 package ch9io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileWriteDemo {
 
-	public static void main(String[] args) throws IOException {
-
+	static void writeData() throws IOException {
 		File file = new File("c://tmp//kunj.data");
 		file.createNewFile();
 
@@ -19,6 +20,47 @@ public class FileWriteDemo {
 		fw.close();
 		System.out.println("Done");
 
+	}
+
+	static void readData() throws IOException {
+		File file = new File("c://tmp//kunj.data");
+		FileReader fr = new FileReader(file);
+
+//		int b = fr.read();// single character read and return unicode { ascii } value
+		
+		int b = 0;
+		
+		while(true) {
+			b=fr.read();
+			if(b == -1) {
+				//-1 end of file 
+				break;
+			}
+			System.out.print((char)b);
+		}
+		
+		fr.close();
+
+	}
+
+	static void readLineData() throws IOException{
+		File file = new File("data.txt");
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+
+		String line ;
+		
+		while( (line= br.readLine())!=null) {
+			System.out.println(line);
+		} 
+		
+		br.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+//		writeData();
+//		readData();
+		readLineData();
 	}
 }
 
