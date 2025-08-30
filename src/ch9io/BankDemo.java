ab7d839574f03352c234c6e4d9e31a48873ecbc3
Tejas Shah <tejasshah2k19@gmail.com>
2025-08-30 13:07:05 +0530

    Serialization Deserialization

---
+++ src/ch9io/BankDemo.java
@@ -0,0 +1,64 @@
+package ch9io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class BankDemo {
+	public static void main(String[] args) throws Exception {
+		Customer c = new Customer();
+		c.firstName = "ram";
+		c.lastName = "patel";
+		c.balance = 58000f;
+		c.email = "ram@gmail.com";
+		c.password = "secret";
+
+		// 500
+		// write 500*5 =>
+		// read
+
+		// single object file ?
+		// serialization => process of writing object state in persistence storage
+
+		// FileWriter
+		// FileOutputStream
+		// PrintWriter
+		// DataOutputStream
+		// BufferedWriter
+
+		FileOutputStream fos = new FileOutputStream("data.ser");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+		oos.writeObject(c);// serialize
+		oos.close();
+
+		FileInputStream fis = new FileInputStream("data.ser");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+
+		Customer c1 = (Customer) ois.readObject();
+		System.out.println(c1.email);
+		System.out.println(c1.password);
+		System.out.println(c1.firstName);
+		System.out.println(c1.lastName);
+		System.out.println(c1.balance);
+		
+		ois.close();
+	}
+}
+
+//empty interface ==> Marker interface 
+class Customer implements Serializable {
+	String firstName;
+	String lastName;
+	String email;
+	String password;
+	Float balance;
+
+	public Customer() {
+		System.out.println("Customer()");
+	}
+
+}
