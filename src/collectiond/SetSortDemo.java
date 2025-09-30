93217966cdab8ec77acacd2800ceb2a9f0e0b8e3
Tejas Shah <tejasshah2k19@gmail.com>
2025-09-30 12:33:19 +0530

    hashcode equals

--- src/collectiond/SetSortDemo.java
+++ src/collectiond/SetSortDemo.java
@@ -1,6 +1,6 @@
 package collectiond;
 
-import java.util.TreeSet;
+import java.util.HashSet;
 
 public class SetSortDemo {
 
@@ -17,16 +17,31 @@ public class SetSortDemo {
 		Student s3 = new Student();
 		s3.name = "Ravan";
 		s3.age = 22;
-		
+
 		Student s4 = new Student();
 		s4.name = "Bharat";
 		s4.age = 18;
+
+		Student s5 = s4; 
 		
-		TreeSet<Student> ts = new TreeSet<Student>();
+		Student s6 = new Student();
+		s6.name = "ram";
+		s6.age = 25;
+		
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		System.out.println(s3.hashCode());
+		System.out.println(s4.hashCode());
+		System.out.println(s5.hashCode());
+		
+		HashSet<Student> ts = new HashSet<Student>();
 		ts.add(s1);
 		ts.add(s2);
 		ts.add(s3);
 		ts.add(s4);
+		
+		ts.add(s5);
+		ts.add(s6);
 		
 		System.out.println(ts);
 
@@ -36,28 +51,37 @@ public class SetSortDemo {
 class Student implements Comparable<Student> {
 	String name;
 	Integer age;
+	 
 	
-	//s1 s2 
+	// s1 s2
 	@Override
 	public int compareTo(Student o) {
-		System.out.println(this.name+" => "+o.name);
+		System.out.println(this.name + " => " + o.name);
 //		return 0;//s1 s2 same 
 //		return -1;
-	
-		if(this.age  > o.age) {
-			return 1;
-		}else if(this.age  < o.age) {
-			return -1;
-		}else {
+
+		if (this.age > o.age) {
 			return 1;
 		}
-	
+		return -1;
+	}
+
+	@Override
+	public int hashCode() {
+		return 1;
 	}
 	
 	@Override
-	public String toString() {
-		return name+":"+age;
+	public boolean equals(Object obj) {
+		Student x = (Student)obj;
+		System.out.println("eq => " + this.name+" : "+x.name);
+//		return super.equals(obj);
+		return this.name.equals(x.name);
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return name + ":" + age;
+	}
+
 }
